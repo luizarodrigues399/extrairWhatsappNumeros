5f2fc50ccbca55775282f26308232d33f5e74607
luizaRodrigues <luiza.rodrigues.cp@gmail.com>
2024-06-22 17:14:59 -0300

    versão que baixa contatos do arquivado e coloca numeração

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+// juntar os arquivos gerados com comando de prompt no windows. Exemplo: "copy /b *.txt newfile.txt"
+
 function saveFile(whatsappContacts){
 
     let filename = 'whatsappContacts.txt'
@@ -46,6 +48,41 @@ const extractContacts = () => {
 }
 
 
+const extractContactsArchived = () => {
+
+    let classes = '.x10l6tqk.x13vifvy.xds687c.x1ey2m1c.x17qophe';
+
+    let gridCell = document.querySelectorAll(classes + " [role='gridcell']._ak8o");
+
+    let span, input, resultado = [];
+
+    gridCell.forEach(function(element, index){
+
+        span = element.querySelector("span");
+
+        input = document.createElement("input");
+
+        input.style='width: 20px; background-color: yellow'; 
+
+        input.type = "text";
+
+        input.value = index;
+
+        element.appendChild(input); 
+
+        if (span.title != 'pronto'){
+            resultado.push(span.title + '\n');
+        }
+
+        span.title = 'pronto';
+    });
+
+    input.style='width: 20px; background-color: red';
+
+    saveFile(resultado)
+
+}
+
 let button = document.createElement("BUTTON")
 
 let text = document.createTextNode("Download Contatos")
@@ -63,7 +100,7 @@ button.style.cssText = css
 
 button.appendChild(text)
 
-button.addEventListener("click", extractContacts)
+button.addEventListener("click", extractContactsArchived)
 
 document.body.appendChild(button)
 
